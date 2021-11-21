7b4e7510dd0e09c4e287f1cf687810aa2c34351d
Faisal Ahmed <itsfaysy@gmail.com>
2021-11-22 01:56:32 +0500

    Attach driver

--- attach_driver.php
+++ attach_driver.php
@@ -31,7 +31,7 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-	<title>Attach Company</title>
+	<title>Attach Driver</title>
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
     <?php require_once('head_inc.php'); ?>  
