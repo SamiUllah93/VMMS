818491681121c7434eaa10439f75311a38638414
Faisal Ahmed <itsfaysy@gmail.com>
2021-11-22 00:34:44 +0500

    Added Company CRUD

---
+++ database/vmss_v4.sql
@@ -0,0 +1,405 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 21, 2021 at 08:34 PM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `vmss`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `company`
+--
+
+CREATE TABLE `company` (
+  `company_id` bigint(20) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `created` date NOT NULL DEFAULT current_timestamp(),
+  `updated` int(11) DEFAULT NULL,
+  `status` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `company`
+--
+
+INSERT INTO `company` (`company_id`, `title`, `created`, `updated`, `status`) VALUES
+(2, 'Bin Qasim', '2021-11-21', NULL, 1),
+(5, 'Googi Khaogy?', '2021-11-21', NULL, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `driver`
+--
+
+CREATE TABLE `driver` (
+  `driver_id` bigint(20) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `armyno` varchar(20) NOT NULL,
+  `created` timestamp(6) NOT NULL DEFAULT current_timestamp(6),
+  `status` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `driver`
+--
+
+INSERT INTO `driver` (`driver_id`, `name`, `armyno`, `created`, `status`) VALUES
+(3, 'Imtiaz Ali', 'PA-987654', '2021-11-05 19:32:59.897449', 1),
+(5, 'Laeeq Rehman', 'PA-45645', '2021-11-06 21:36:28.490528', 1),
+(6, 'Anees Ahmad Khan', 'PA-5467', '2021-11-06 21:36:38.005154', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fuel_record`
+--
+
+CREATE TABLE `fuel_record` (
+  `fuel_id` bigint(20) NOT NULL,
+  `vehicle_id` bigint(20) NOT NULL,
+  `total_fuel_added` decimal(10,0) NOT NULL,
+  `total_running` decimal(10,0) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `fuel_record`
+--
+
+INSERT INTO `fuel_record` (`fuel_id`, `vehicle_id`, `total_fuel_added`, `total_running`, `created`, `status`) VALUES
+(1, 13, '13', '365', '2021-11-06 22:39:54', 0),
+(2, 13, '33', '655', '2021-11-06 22:39:54', 0),
+(3, 13, '35', '56', '2021-11-07 21:30:10', 0),
+(4, 13, '55', '395', '2021-11-07 21:30:38', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `maintenance`
+--
+
+CREATE TABLE `maintenance` (
+  `maintenance_id` bigint(50) NOT NULL,
+  `title` varchar(80) NOT NULL,
+  `created` timestamp(6) NOT NULL DEFAULT current_timestamp(6),
+  `status` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `maintenance`
+--
+
+INSERT INTO `maintenance` (`maintenance_id`, `title`, `created`, `status`) VALUES
+(1, 'Engine Oil Changed', '2021-11-05 19:39:07.885281', 0),
+(2, 'Brake Oil Change', '2021-11-05 19:39:40.453604', 0),
+(3, 'Clutch Set Update', '2021-11-05 19:39:54.421050', 0),
+(4, 'Tire Replacement', '2021-11-05 19:40:03.601611', 0),
+(5, 'Oil Filter Change', '2021-11-05 19:40:16.113508', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `maintenance_vehicle`
+--
+
+CREATE TABLE `maintenance_vehicle` (
+  `maintenance_vehicle_ID` bigint(50) NOT NULL,
+  `maintenance_ID` bigint(50) NOT NULL,
+  `vehicle_ID` bigint(50) NOT NULL,
+  `duration_In_days` int(50) NOT NULL,
+  `distance` int(50) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `next_due` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `maintenance_vehicle`
+--
+
+INSERT INTO `maintenance_vehicle` (`maintenance_vehicle_ID`, `maintenance_ID`, `vehicle_ID`, `duration_In_days`, `distance`, `status`, `next_due`) VALUES
+(1, 1, 13, 90, 3000, 0, '2022-02-06'),
+(2, 3, 13, 700, 15000, 0, '2023-10-09'),
+(3, 5, 13, 90, 3500, 0, '0000-00-00'),
+(6, 2, 15, 365, 15000, 0, '0000-00-00'),
+(7, 3, 15, 123, 3500, 0, '2022-03-11'),
+(8, 1, 16, 125, 520, 0, '2022-03-26');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parts`
+--
+
+CREATE TABLE `parts` (
+  `part_id` bigint(50) NOT NULL,
+  `title` varchar(20) NOT NULL,
+  `vehicle_ID` bigint(20) NOT NULL,
+  `Added_date` datetime(6) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `parts`
+--
+
+INSERT INTO `parts` (`part_id`, `title`, `vehicle_ID`, `Added_date`, `created`) VALUES
+(1, 'Clutch Bearing', 13, '2021-11-08 00:00:00.000000', '2021-11-07 21:12:40'),
+(3, 'Headlight Bulbs', 15, '2020-10-07 00:00:00.000000', '2021-11-07 21:15:02');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `process_maintenance`
+--
+
+CREATE TABLE `process_maintenance` (
+  `process_maintenance_id` bigint(50) NOT NULL,
+  `maintenance_vehicleid` bigint(50) NOT NULL,
+  `odometer_reading` int(50) NOT NULL,
+  `created` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6),
+  `Status` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `process_maintenance`
+--
+
+INSERT INTO `process_maintenance` (`process_maintenance_id`, `maintenance_vehicleid`, `odometer_reading`, `created`, `Status`) VALUES
+(11, 1, 123465, '2021-11-07 19:16:09.088748', 0),
+(12, 1, 123465, '2021-11-07 19:17:08.963149', 0),
+(13, 7, 123465, '2021-11-07 19:18:26.737323', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` bigint(20) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `created` timestamp NULL DEFAULT current_timestamp(),
+  `updated` timestamp NOT NULL DEFAULT current_timestamp(),
+  `isadmin` tinyint(1) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `name`, `email`, `password`, `created`, `updated`, `isadmin`, `status`) VALUES
+(1, 'Faisal', 'faisal@gmail.com', 'BetaHouse123#', '2021-11-05 17:55:45', '2021-11-05 17:55:45', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vehicle`
+--
+
+CREATE TABLE `vehicle` (
+  `Vehicle_ID` bigint(50) NOT NULL,
+  `BA_NO` varchar(50) NOT NULL,
+  `Make_Type` varchar(50) NOT NULL,
+  `Issued_On` date NOT NULL,
+  `Year_of_Manufacturer` int(6) NOT NULL,
+  `Driver_ID` bigint(50) DEFAULT NULL,
+  `company_id` bigint(20) DEFAULT NULL,
+  `created` timestamp(6) NOT NULL DEFAULT current_timestamp(6),
+  `Status` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `vehicle`
+--
+
+INSERT INTO `vehicle` (`Vehicle_ID`, `BA_NO`, `Make_Type`, `Issued_On`, `Year_of_Manufacturer`, `Driver_ID`, `company_id`, `created`, `Status`) VALUES
+(13, 'AVG-790', 'Suzuki Alto', '2020-10-05', 2021, 3, NULL, '2021-11-05 20:56:36.844794', 0),
+(15, 'SV-308', 'Honda City', '2020-10-06', 2021, 5, NULL, '2021-11-06 21:41:11.419590', 0),
+(16, 'AFF-021', 'Suzuki', '2022-12-22', 2001, 6, 2, '2021-11-21 18:20:09.737746', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `company`
+--
+ALTER TABLE `company`
+  ADD PRIMARY KEY (`company_id`);
+
+--
+-- Indexes for table `driver`
+--
+ALTER TABLE `driver`
+  ADD PRIMARY KEY (`driver_id`);
+
+--
+-- Indexes for table `fuel_record`
+--
+ALTER TABLE `fuel_record`
+  ADD PRIMARY KEY (`fuel_id`),
+  ADD KEY `vehicle_id_fk` (`vehicle_id`);
+
+--
+-- Indexes for table `maintenance`
+--
+ALTER TABLE `maintenance`
+  ADD PRIMARY KEY (`maintenance_id`);
+
+--
+-- Indexes for table `maintenance_vehicle`
+--
+ALTER TABLE `maintenance_vehicle`
+  ADD PRIMARY KEY (`maintenance_vehicle_ID`),
+  ADD KEY `maintenance_id-fk` (`maintenance_ID`),
+  ADD KEY `vehicle_id_fk` (`vehicle_ID`);
+
+--
+-- Indexes for table `parts`
+--
+ALTER TABLE `parts`
+  ADD PRIMARY KEY (`part_id`),
+  ADD KEY `vehicle_ID` (`vehicle_ID`);
+
+--
+-- Indexes for table `process_maintenance`
+--
+ALTER TABLE `process_maintenance`
+  ADD PRIMARY KEY (`process_maintenance_id`),
+  ADD KEY `process_maintenance_fk` (`maintenance_vehicleid`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `vehicle`
+--
+ALTER TABLE `vehicle`
+  ADD PRIMARY KEY (`Vehicle_ID`),
+  ADD KEY `Driver_ID_FK` (`Driver_ID`),
+  ADD KEY `company_id` (`company_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `company`
+--
+ALTER TABLE `company`
+  MODIFY `company_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `driver`
+--
+ALTER TABLE `driver`
+  MODIFY `driver_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `fuel_record`
+--
+ALTER TABLE `fuel_record`
+  MODIFY `fuel_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `maintenance`
+--
+ALTER TABLE `maintenance`
+  MODIFY `maintenance_id` bigint(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `maintenance_vehicle`
+--
+ALTER TABLE `maintenance_vehicle`
+  MODIFY `maintenance_vehicle_ID` bigint(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `parts`
+--
+ALTER TABLE `parts`
+  MODIFY `part_id` bigint(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `process_maintenance`
+--
+ALTER TABLE `process_maintenance`
+  MODIFY `process_maintenance_id` bigint(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `vehicle`
+--
+ALTER TABLE `vehicle`
+  MODIFY `Vehicle_ID` bigint(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `fuel_record`
+--
+ALTER TABLE `fuel_record`
+  ADD CONSTRAINT `fuel_record_ibfk_1` FOREIGN KEY (`vehicle_id`) REFERENCES `vehicle` (`Vehicle_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `maintenance_vehicle`
+--
+ALTER TABLE `maintenance_vehicle`
+  ADD CONSTRAINT `maintenance_vehicle_ibfk_1` FOREIGN KEY (`maintenance_ID`) REFERENCES `maintenance` (`maintenance_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `maintenance_vehicle_ibfk_2` FOREIGN KEY (`vehicle_ID`) REFERENCES `vehicle` (`Vehicle_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `parts`
+--
+ALTER TABLE `parts`
+  ADD CONSTRAINT `parts_ibfk_1` FOREIGN KEY (`vehicle_ID`) REFERENCES `vehicle` (`Vehicle_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `process_maintenance`
+--
+ALTER TABLE `process_maintenance`
+  ADD CONSTRAINT `process_maintenance_ibfk_1` FOREIGN KEY (`maintenance_vehicleid`) REFERENCES `maintenance_vehicle` (`maintenance_vehicle_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `vehicle`
+--
+ALTER TABLE `vehicle`
+  ADD CONSTRAINT `vehicle_ibfk_1` FOREIGN KEY (`Driver_ID`) REFERENCES `driver` (`driver_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `vehicle_ibfk_2` FOREIGN KEY (`company_id`) REFERENCES `company` (`company_id`) ON DELETE SET NULL ON UPDATE SET NULL;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
